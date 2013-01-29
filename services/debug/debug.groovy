1e69203d084040bb2ada61a290b3a0f788471a3f
Yoni Yalovitsky <yoni@fewbytes.com>
2013-01-30 00:54:32 +0200

    changed debug args syntax and added a few new ones

--- services/debug/debug.groovy
+++ services/debug/debug.groovy
@@ -6,31 +6,32 @@ import org.cloudifysource.dsl.context.ServiceContext
 
 def debug(args) {
     def cli = new CliBuilder(usage: "${this.class.name}.groovy OPTION")
-    cli.with {
-        h longOpt: 'help', 'Show usage information'
-        c longOpt: 'print-context', 'Print the cloudify context for this instance'
-        a longOpt: 'list-attributes', 'Output cloudify attributes (global/application/service/instance)'
-        s longOpt: 'script-info', 'Output the variables and properties for the current script'
-        e longOpt: 'edit-script', 'Edit the current script'
-        r longOpt: 'run-script', 'Run the current script'
-        g longOpt: 'run-groovy', 'Run a groovy command in this context'
-    }
+    cli._(longOpt:'help', 'Show usage information')
+    cli._(longOpt:'print-context', 'Print the cloudify context for this instance')
+    cli._(longOpt:'list-attributes', args:1, argName:'scope', 'Output cloudify attributes (global/application/service/instance)')
+    cli._(longOpt:'script-info', 'Output the variables and properties for the current script')
+    cli._(longOpt:'edit-script', 'Edit the current script')
+    cli._(longOpt:'run-script', 'Run the current script')
+    cli._(longOpt:'run-groovy', 'Run a groovy command in this context')
+    cli._(longOpt:'launch-groovysh', 'Launch a groovy shell with this context')
+    cli._(longOpt:'exit', 'Exit debugging shell (with option of returning)')
+    cli._(longOpt:'finish', 'Finish debugging (move to next lifecycle event)')
     def options = cli.parse(args)
 
-    if (args.size() == 0 || !options || options.h) { //show usage text
+    if (args.size() == 0 || !options || options.'help') { //show usage text
         cli.usage()
         return
     }
 
     ServiceContext context = ServiceContextFactory.getServiceContext()
 
-    if (options.c) {
+    if (options.'print-context') {
         println(context.toString())
         //TODO: print all the properties in the context
         return
     }
 
-    if (options.a) {
+    if (options.'list-attributes') {
         //for (attr in context.attributes.thisInstance) {println attr}
         for (attr in context.attributes.global) {println attr}
         return
