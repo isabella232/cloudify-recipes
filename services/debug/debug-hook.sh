20631ebebc417afddbae8a3bb8a1dca199cb9e3f
Yoni Yalovitsky <yoni@fewbytes.com>
2013-01-30 15:57:42 +0200

    fixed debug alias generation

--- services/debug/debug-hook.sh
+++ services/debug/debug-hook.sh
@@ -1,36 +1,42 @@
 #! /bin/bash
 
-WORKDIR="$HOME/gigaspaces/work/processing-units/${USM_APPLICATION_NAME}_${USM_SERVICE_NAME}_${USM_INSTANCE_ID}/ext"
+export SCRIPT_NAME=$1
+export CLOUDIFY_WORKDIR="$HOME/gigaspaces/work/processing-units/${USM_APPLICATION_NAME}_${USM_SERVICE_NAME}_${USM_INSTANCE_ID}/ext"
 #prepare an rc file for the debug shell
 :>$HOME/debugrc
 function write_script () {
     echo >>$HOME/debugrc "$@"
 }
+write_script 'echo "Loading the debug environment..."'
 write_script '#the cloudify environment variables for this event:'
 printenv | grep -E "^(CLOUDIFY|USM|LOOKUP)" | while read var; do write_script export $var; done
 write_script ''
-write_script "cd $WORKDIR"
+write_script "cd $CLOUDIFY_WORKDIR"
 write_script 'export JAVA_HOME=$HOME/java'
 write_script 'export CLASSPATH=`find $HOME/gigaspaces -name *.jar | paste -sd:`'
 write_script 'export PATH=$HOME/gigaspaces/tools/groovy/bin:$PATH'
 
 write_script "chmod +x debug.groovy"
-write_script "chmod +x $1"
-write_script "PS1='Debugging[$1]: '"
+write_script "chmod +x $SCRIPT_NAME"
+write_script "PS1='Debugging[$SCRIPT_NAME]: '"
 
 #set up shortcut aliases
-$WORKDIR/debug.groovy | grep -Eo '\-\-[^ ]*' | cut -c3- | \
-    xargs -l -I{} write_script "alias {}=\"$WORKDIR/debug.groovy --{}\""
+write_script '[[ -f debug_commands ]] || ./debug.groovy | tail -n+2 >debug_commands'
+write_script 'for COMMAND in `grep -Eo "\-\-[^ ]*" debug_commands | cut -c3- `; do
+                  alias $COMMAND="$CLOUDIFY_WORKDIR/debug.groovy --$COMMAND" 
+              done'
+
 #some special treatment for the help alias
-write_script "help= \"$WORKDIR/debug.groovy | tail -n+2 | cut -c7- \""
+write_script "alias help=\"cut -c7- <$CLOUDIFY_WORKDIR/debug_commands\""
 
 #a nice intro
-write_script ''
+write_script 'clear'
 write_script 'echo -n "Starting a debugging session"'
-[[ -n "$1"  ]] && write_script "echo ' for hook $1'" || write_script "echo ''"
+[[ -n "$SCRIPT_NAME"  ]] && write_script "echo ' for hook $SCRIPT_NAME'" || write_script "echo ''"
 write_script ''
 write_script 'echo "These are the available debug commands:"'
 write_script 'help' #use our newly created alias
+write_script 'echo'
 
 #set up the "debug" alias to enter the debug shell
 if ! alias debug &>/dev/null ; then
@@ -40,8 +46,8 @@ fi
 
 #Enter a loop to avoid proceeding to the next step until the user finished debugging
 :>$HOME/debugging
-while [[ -f $HOME/debugging ]]; do
-    echo "The service $USM_SERVICE_NAME (script $1) is waiting to be debugged on $CLOUDIFY_AGENT_ENV_PUBLIC_IP."
-    echo " When finished, delete the file $HOME/debugging (or use the 'finish' debug command)"
+while [[ -f $HOME/.cloudify_debugging ]]; do
+    echo "The service $USM_SERVICE_NAME (script $SCRIPT_NAME) is waiting to be debugged on $CLOUDIFY_AGENT_ENV_PUBLIC_IP."
+    echo " When finished, delete the file $HOME/.cloudify_debugging (or use the 'finish' debug command)"
     sleep 60
 done
