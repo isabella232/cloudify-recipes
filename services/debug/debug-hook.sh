5bc3351ff7ad11800f496c6a2be321bf9be7fc6f
Yoni Yalovitsky <yoni@fewbytes.com>
2013-02-13 00:15:47 +0200

    exported additional variables to the debug environment

--- services/debug/debug-hook.sh
+++ services/debug/debug-hook.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-export SCRIPT_NAME=$1
-export CLOUDIFY_WORKDIR="$HOME/gigaspaces/work/processing-units/${USM_APPLICATION_NAME}_${USM_SERVICE_NAME}_${USM_INSTANCE_ID}/ext"
+SCRIPT_NAME=$1
+CLOUDIFY_WORKDIR="$HOME/gigaspaces/work/processing-units/${USM_APPLICATION_NAME}_${USM_SERVICE_NAME}_${USM_INSTANCE_ID}/ext"
+KEEPALIVE_FILE=$HOME/.cloudify_debugging
 #prepare an rc file for the debug shell
 :>$HOME/debugrc
 function write_script () {
@@ -12,7 +13,10 @@ write_script 'echo "Loading the debug environment..."'
 write_script '#the cloudify environment variables for this event:'
 printenv | grep -E "^(CLOUDIFY|USM|LOOKUP)" | while read var; do write_script export $var; done
 write_script ''
-write_script "cd $CLOUDIFY_WORKDIR"
+write_script "export CLOUDIFY_WORKDIR=$CLOUDIFY_WORKDIR"
+write_script "export SCRIPT_NAME=$SCRIPT_NAME"
+write_script "export KEEPALIVE_FILE=$KEEPALIVE_FILE"
+write_script 'cd $CLOUDIFY_WORKDIR'
 write_script 'export JAVA_HOME=$HOME/java'
 write_script 'export CLASSPATH=`find $HOME/gigaspaces/lib/{required,platform/cloudify} -name *.jar | paste -sd:`'
 write_script 'export PATH=$HOME/gigaspaces/tools/groovy/bin:$PATH'
@@ -28,12 +32,12 @@ write_script 'for COMMAND in `grep -Eo "\-\-[^ ]*" debug_commands | cut -c3- `; 
               done'
 
 #some special treatment for the help alias
-write_script "alias help=\"cut -c7- <$CLOUDIFY_WORKDIR/debug_commands\""
+write_script 'alias help="cut -c7- <$CLOUDIFY_WORKDIR/debug_commands"'
 
 #a nice intro
 write_script 'clear'
 write_script 'echo -n "Starting a debugging session"'
-[[ -n "$SCRIPT_NAME"  ]] && write_script "echo ' for hook $SCRIPT_NAME'" || write_script "echo ''"
+[[ -n "$SCRIPT_NAME"  ]] && write_script 'echo ": for hook $SCRIPT_NAME"' || write_script "echo ''"
 write_script ''
 write_script 'echo "These are the available debug commands:"'
 write_script 'help' #use our newly created alias
@@ -46,9 +50,9 @@ if ! alias debug &>/dev/null ; then
 fi
 
 #Enter a loop to avoid proceeding to the next step until the user finished debugging
-:>$HOME/debugging
-while [[ -f $HOME/.cloudify_debugging ]]; do
+:>$KEEPALIVE_FILE
+while [[ -f $KEEPALIVE_FILE ]]; do
     echo "The service $USM_SERVICE_NAME (script $SCRIPT_NAME) is waiting to be debugged on $CLOUDIFY_AGENT_ENV_PUBLIC_IP."
-    echo " When finished, delete the file $HOME/.cloudify_debugging (or use the 'finish' debug command)"
+    echo "When finished, delete the file $KEEPALIVE_FILE (or use the 'finish' debug command)"
     sleep 60
 done
