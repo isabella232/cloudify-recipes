bee84c17e5579997c677c0ea67c03b3d7958e35c
Yoni Yalovitsky <yoni@fewbytes.com>
2013-02-13 00:15:58 +0200

    some work on debug commands
    I added the Shell library but it seems it won't suit here since we'd
    rather groovy to just die and syscall exec. But it seems that's not
    something the jvm is good for.
    I suppose that it'll be best to just move everything that doesn't need
    the context out of the groovy script into bash.

--- services/debug/debug.groovy
+++ services/debug/debug.groovy
@@ -3,6 +3,12 @@
 import java.text.*
 import org.cloudifysource.dsl.context.ServiceContextFactory
 import org.cloudifysource.dsl.context.ServiceContext
+import static Shell.* //This is apparently not the way to go
+
+def getScriptPath() {
+    def env = System.getenv()
+    return pathJoin(env["CLOUDIFY_WORKDIR"], env["SCRIPT_NAME"])
+}
 
 def debug(args) {
     def cli = new CliBuilder(usage: "${this.class.name}.groovy OPTION")
@@ -12,7 +18,7 @@ def debug(args) {
     cli._(longOpt:'script-info', 'Output the variables and properties for the current script')
     cli._(longOpt:'edit-script', 'Edit the current script')
     cli._(longOpt:'run-script', 'Run the current script')
-    cli._(longOpt:'run-groovy', 'Run a groovy command in this context')
+    cli._(longOpt:'run-groovy', args:1, argName:'command', 'Run a groovy command with this context as "x"')
     cli._(longOpt:'launch-groovysh', 'Launch a groovy shell with this context')
     cli._(longOpt:'finish', 'Finish debugging (move to next lifecycle event)')
     def options = cli.parse(args)
@@ -33,6 +39,38 @@ def debug(args) {
     if (options.'list-attributes') {
         //for (attr in context.attributes.thisInstance) {println attr}
         for (attr in context.attributes.global) {println attr}
+        return
+    }
+
+    if (options.'script-info') {
+
+        return
+    }
+
+    if (options.'edit-script') {
+        sh("vim ${getScriptPath()}") //TODO: This is broken - I can't get groovy to return the tty
+        return
+    }
+
+    if (options.'run-script') {
+        sh(getScriptPath()) //TODO: broken - get this to process the groovy shebang
+        return
+    }
+
+    if (options.'run-groovy') {
+        Binding binding = new Binding();
+        binding.setVariable("context", context);
+        new GroovyShell(binding).evaluate(options.'run-groovy')
+        return
+    }
+
+    if (options.'launch-groovysh') {
+        sh('$HOME/gigaspaces/tools/groovy/bin/groovysh -q') //TODO: This is broken - I can't get groovy to return the tty
+        return
+    }
+
+    if (options.'finish') {
+        new File(System.getenv()['KEEPALIVE_FILE']).delete()
         return
     }
 }
