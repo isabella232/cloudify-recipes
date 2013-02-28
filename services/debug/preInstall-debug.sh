32de1ac65346dc942daefa7a6408dd1a4063aa3a
Yoni Yalovitsky <yoni@fewbytes.com>
2013-02-28 12:19:49 +0200

    progress, but file checks aren't working

--- services/debug/preInstall-debug.sh
+++ services/debug/preInstall-debug.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#TODO: push this logic into the jar
+#TODO: push this logic into the jar - DELETE this and the service mention after it's cool
 
 #set up an ssh key for the debugging connection
-if [[ -f debugPublicKey ]]
-then
-    cat debugPublicKey >>$HOME/.ssh/authorized_keys
-fi
+#if [[ -f debugPublicKey ]]
+#then
+#    cat debugPublicKey >>$HOME/.ssh/authorized_keys
+#fi
